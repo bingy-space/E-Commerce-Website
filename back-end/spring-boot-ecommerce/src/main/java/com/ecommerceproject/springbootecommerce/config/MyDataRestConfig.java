64aa4007001253a719e5525cc5f68c29461057d9
BingNa Yang <bing334@gmail.com>
2023-09-28 17:43:41 -0400

    Setup MyDataRestConfig
    Configure for Read Only.
    Unsupported action:  PUT, POST, and DELETE

---
+++ back-end/spring-boot-ecommerce/src/main/java/com/ecommerceproject/springbootecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,28 @@
+package com.ecommerceproject.springbootecommerce.config;
+
+import com.ecommerceproject.springbootecommerce.entity.Product;
+import com.ecommerceproject.springbootecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        // Disable HTT methods for Product: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        // Disable HTT methods for ProductCategory: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
