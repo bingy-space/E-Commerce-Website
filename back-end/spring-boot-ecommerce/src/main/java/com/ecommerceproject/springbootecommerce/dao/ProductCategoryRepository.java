75d00c3d8a61e2cd8fe02d6d5e08c6120414638f
BingNa Yang <bing334@gmail.com>
2023-09-28 16:01:24 -0400

    Setup Product Repository and Product Category Repository Interface for REST APIs
    Create REST APIs with Spring Data JPA Repositories and Spring Data REST

---
+++ back-end/spring-boot-ecommerce/src/main/java/com/ecommerceproject/springbootecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerceproject.springbootecommerce.dao;
+
+import com.ecommerceproject.springbootecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path="product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
