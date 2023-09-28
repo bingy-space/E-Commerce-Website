8efe27dbf3cf0a4ebaf0714e392d90e5e1dfe125
BingNa Yang <bing334@gmail.com>
2023-09-28 19:53:06 -0400

    Add CrossOrigin support to Spring Boot

--- back-end/spring-boot-ecommerce/src/main/java/com/ecommerceproject/springbootecommerce/dao/ProductRepository.java
+++ back-end/spring-boot-ecommerce/src/main/java/com/ecommerceproject/springbootecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.ecommerceproject.springbootecommerce.dao;
 
 import com.ecommerceproject.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
