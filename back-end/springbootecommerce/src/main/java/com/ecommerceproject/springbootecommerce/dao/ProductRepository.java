cc729e7a2f1452fd7999cd99ca2d330d48598398
Bing <bing334@gmail.com>
2023-10-03 23:44:51 -0400

    Update backend folder path and add new method to ProductRepository
    - findByCategoryId

--- back-end/springbootecommerce/src/main/java/com/ecommerceproject/springbootecommerce/dao/ProductRepository.java
+++ back-end/springbootecommerce/src/main/java/com/ecommerceproject/springbootecommerce/dao/ProductRepository.java
@@ -1,10 +1,15 @@
 package com.ecommerceproject.springbootecommerce.dao;
 
 import com.ecommerceproject.springbootecommerce.entity.Product;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.data.repository.query.Param;
+
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
