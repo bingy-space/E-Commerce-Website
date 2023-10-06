d7ffa926f5d4939598125c28f52c311418174bc2
Bing <bing334@gmail.com>
2023-10-06 17:51:37 -0400

    Add route for search and update ProductService to call URL for search by keyword

--- front-end/angular-ecommerce/src/app/services/product.service.ts
+++ front-end/angular-ecommerce/src/app/services/product.service.ts
@@ -18,9 +18,8 @@ export class ProductService {
     // Need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response => response._embedded.products)
-    )
+    return this.getProducts(searchUrl);
+
   }
 
   getProductCategories(): Observable<ProductCategory[]>{
@@ -28,6 +27,20 @@ export class ProductService {
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
     )
+  }
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    // Need to build URL based on category id
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchUrl)
+
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    );
   }
 }
 
