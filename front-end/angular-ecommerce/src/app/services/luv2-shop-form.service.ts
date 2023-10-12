2d0e855a02aafb169e9e386fb3d6fcd46ea5aa8c
Bing <bing334@gmail.com>
2023-10-12 12:16:21 -0400

    Add methods for countries and states
    - getCountries()
    - getStates()

--- front-end/angular-ecommerce/src/app/services/luv2-shop-form.service.ts
+++ front-end/angular-ecommerce/src/app/services/luv2-shop-form.service.ts
@@ -1,16 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
+import { Country } from '../common/country';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Luv2ShopFormService {
 
-  constructor() { }
+  private countriesUrl = "http://localhost:8080/api/countries";
+  private statesUrl = "http://localhost:8080/api/states";
+
+  constructor(private httpClient: HttpClient) { }
 
   getCreditCardMonths(startMonth: number): Observable<number[]>{
     let data: number[] = [];
-
 
     // Build an array for "Month" dropdown list
     // Start at current month and loop until
@@ -35,5 +40,31 @@ export class Luv2ShopFormService {
     }
 
     return of(data);
+  }
+
+  getCountries(): Observable<Country[]>{
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(theCountryCode: string): Observable<State[]>{
+    // Search url
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    )
+  }
+}
+
+interface GetResponseCountries{
+  _embedded: {
+    countries: Country[];
+  }
+}
+interface GetResponseStates{
+  _embedded: {
+    states: State[];
   }
 }
