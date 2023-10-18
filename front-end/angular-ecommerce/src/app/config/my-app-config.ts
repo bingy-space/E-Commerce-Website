c229a5625f5d53d35f9c9bfb48bcb9c32c2e3b0e
Bing <bing334@gmail.com>
2023-10-18 17:55:06 -0400

    update to export default

--- front-end/angular-ecommerce/src/app/config/my-app-config.ts
+++ front-end/angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
-export interface MyAppConfig {
+export default {
     oidc: {
-        clientId:'',
-        issuer: '',
+        clientId:'0oacu61fwwn7MDjDn5d7',
+        issuer: 'https://dev-58074233.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid','profile','email']
     }
