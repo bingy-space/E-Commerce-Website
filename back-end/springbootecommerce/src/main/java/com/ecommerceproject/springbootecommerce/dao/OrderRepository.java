13b0efd2f306e107988fa852edbff6dd127c1cc1
Bing <bing334@gmail.com>
2023-10-20 15:42:34 -0400

    Sort orders by date and populate user's email address in the form

--- back-end/springbootecommerce/src/main/java/com/ecommerceproject/springbootecommerce/dao/OrderRepository.java
+++ back-end/springbootecommerce/src/main/java/com/ecommerceproject/springbootecommerce/dao/OrderRepository.java
@@ -10,5 +10,5 @@ import com.ecommerceproject.springbootecommerce.entity.Order;
 
 @RepositoryRestResource
 public interface OrderRepository extends JpaRepository<Order, Long>{
-	Page<Order> findByCustomerEmail(@Param("email") String email, Pageable pageable);
+	Page<Order> findByCustomerEmailOrderByDateCreatedDesc(@Param("email") String email, Pageable pageable);
 }
