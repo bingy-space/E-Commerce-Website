07c1e70a89e46294528dc5a4aab65c781d5b36eb
Bing <bing334@gmail.com>
2023-10-20 13:41:09 -0400

    fixed typo

--- back-end/springbootecommerce/src/main/java/com/ecommerceproject/springbootecommerce/config/MyAppConfig.java
+++ back-end/springbootecommerce/src/main/java/com/ecommerceproject/springbootecommerce/config/MyAppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyAppConfig implements WebMvcConfigurer{
 	
-	@Value("${allowed.orgins}")
+	@Value("${allowed.origins}")
 	private String[] theAllowedOrigins;
 	
 	@Value("${spring.data.rest.base-path}")
