92d4827a22264df9f7af39b36a7262189642e604
Bing <bing334@gmail.com>
2023-10-19 20:48:39 -0400

    Add new method: findByEmail()

--- back-end/springbootecommerce/src/main/java/com/ecommerceproject/springbootecommerce/dao/CustomerRepository.java
+++ back-end/springbootecommerce/src/main/java/com/ecommerceproject/springbootecommerce/dao/CustomerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ecommerceproject.springbootecommerce.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
-
+	Customer findByEmail(String theEmail);
 }
