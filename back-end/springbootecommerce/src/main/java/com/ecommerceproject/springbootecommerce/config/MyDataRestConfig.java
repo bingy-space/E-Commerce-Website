a6683e2e9a45f31166e44bf5f754a0cf1677dd1a
Bing <bing334@gmail.com>
2023-10-20 13:30:35 -0400

    Configure CORS mapping for Spring Data REST

--- back-end/springbootecommerce/src/main/java/com/ecommerceproject/springbootecommerce/config/MyDataRestConfig.java
+++ back-end/springbootecommerce/src/main/java/com/ecommerceproject/springbootecommerce/config/MyDataRestConfig.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -21,6 +22,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+	
+	@Value("${allowed.origins}")
+	private String[] theAllowedOrigins;
 	
 	private EntityManager entityManager;
 	
@@ -33,13 +37,15 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
-        // Disable HTT methods for Product, ProductCategory, Country, and State: PUT, POST and DELETE
+        // Disable HTTP methods for Product, ProductCategory, Country, and State: PUT, POST and DELETE
         disableHttpMethods(Product.class, config, theUnsupportedActions);
         disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
         disableHttpMethods(State.class, config, theUnsupportedActions);
         disableHttpMethods(Country.class, config, theUnsupportedActions);
         // Call an internal helper method
         exposeIds(config);
+        // configure cors mapping
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
     }
 
 	private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
--- back-end/springbootecommerce/src/main/java/com/ecommerceproject/springbootecommerce/dao/ProductRepository.java
+++ back-end/springbootecommerce/src/main/java/com/ecommerceproject/springbootecommerce/dao/ProductRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.data.repository.query.Param;
 
-
-@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
     Page<Product> findByNameContaining(@Param("name") String name, Pageable page);
