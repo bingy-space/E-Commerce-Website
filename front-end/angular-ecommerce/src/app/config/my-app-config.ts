d704c098b17206c152110a0dec292762bb7b94a0
Bing <bing334@gmail.com>
2023-10-21 18:08:08 -0400

    update redirect URI

--- front-end/angular-ecommerce/src/app/config/my-app-config.ts
+++ front-end/angular-ecommerce/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId:'0oacu61fwwn7MDjDn5d7',
         issuer: 'https://dev-58074233.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid','profile','email']
     }
 }
